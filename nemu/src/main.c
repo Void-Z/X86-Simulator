2bcd1d31ce7fdbdd12100221e12edf99231be8d7
tracer-ics2018 <tracer@njuics.org>
2018-12-10 17:50:49 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-29-generic #31-Ubuntu SMP Tue Jul 17 15:39:52 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:50:49 up 4 min,  1 user,  load average: 1.14, 0.97, 0.45 a702a7c6ca1767ec99f1d677ad0263f35c174298

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -8,14 +8,39 @@
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
+struct {
+  unsigned int val;
+  char expr_buf[65536];
+  unsigned int error;
+  unsigned int total;
+} expr_test_unit;
+
+void expr_test() {
+  printflog("Open test file ...\n");
+  FILE *fp = fopen("test.txt", "rt");
+  assert(fp != NULL);
+  printflog("Open success!\n");
+  while(fscanf(fp, "%d %s\n", &expr_test_unit.val, expr_test_unit.expr_buf) != EOF) {
+    printf("%d %s\n",expr_test_unit.val,expr_test_unit.expr_buf);
+    ++expr_test_unit.total;
+    bool * success = (bool *)malloc(sizeof(bool));
+    if(expr_test_unit.val != expr(expr_test_unit.expr_buf,success)) {
+      ++expr_test_unit.error;
+    }
+    free(success);
+  }
+  fclose(fp);
+  printflog("Expr() test results : total %d test(s), %d error(s)\n",expr_test_unit.total,expr_test_unit.error);
+}
 
 
 int main(int argc, char *argv[]) {
-  /* Test expr(). */
-  
-
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
+
+  /* Test expr(). */
+  
+  expr_test();
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
