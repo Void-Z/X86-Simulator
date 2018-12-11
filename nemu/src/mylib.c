a42728d2927047364a66d08994b770b20037c34f
tracer-ics2018 <tracer@njuics.org>
2018-12-11 20:02:52 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-29-generic #31-Ubuntu SMP Tue Jul 17 15:39:52 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:02:52 up  4:19,  1 user,  load average: 0.48, 0.32, 0.24 a6380b781dbe098927a20886d95651d36060e22e

--- nemu/src/mylib.c
+++ nemu/src/mylib.c
@@ -19,13 +19,14 @@ int mylib_log(const char * format, ...) {
 }
 
 uint32_t mylib_atox(char *args) {
-  uint32_t val = 0;
-  args += 2;
-  for(;*args;++args) {
-    val *= 16;
-    val += *args >= 'a' ? *args - 'a' + 10 : *args - '0';
-  }
-  return val;
+    _.log("%s",args);
+    uint32_t val = 0;
+    args += 2;
+    for(;*args;++args) {
+        val *= 16;
+        val += *args >= 'a' ? *args - 'a' + 10 : *args - '0';
+    }
+    return val;
 }
 
 uint32_t mylib_getreg(char *args,bool *success) {
