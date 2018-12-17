437a26359f06f807c131c46e18f44e13d27f4162
tracer-ics2018 <tracer@njuics.org>
2018-12-17 10:23:13 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:23:13 up  1:32,  1 user,  load average: 0.26, 0.27, 0.25 1b1a2628fca271e6cbb730b4ed73b0d63b925aea

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -257,5 +257,5 @@ void exec_wrapper(bool print_flag) {
   printflog("edx : 0x%08x ,esi : 0x%08x\n",cpu.edx,cpu.esi);
   printflog("ebx : 0x%08x ,edi : 0x%08x\n",cpu.ebx,cpu.edi);
   printflog("eip : 0x%08x\n",cpu.eip);
-  printflog("stack top : 0x%08x\n",*(uint32_t *)guest_to_host(cpu.eip));
+  printflog("stack top : 0x%08x\n",*(uint32_t *)guest_to_host(cpu.esp % PMEM_SIZE));
 }
