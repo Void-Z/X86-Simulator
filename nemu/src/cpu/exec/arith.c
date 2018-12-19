b9be1355191c8b12149fd2c45c76bf41a445d927
tracer-ics2018 <tracer@njuics.org>
2018-12-19 16:29:30 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:29:30 up  4:18,  1 user,  load average: 0.25, 0.19, 0.20 8e0785e6ded57a0454095651f39fe75572526db0

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -1,9 +1,7 @@
 #include "cpu/exec.h"
 
 make_EHelper(add) {
-  printf("0x%08x,0x%08x\n",id_src->val,id_dest->val);
   rtl_sext(&id_src->val,&id_src->val,id_src->width);
-  printf("0x%08x,0x%08x\n",id_src->val,id_dest->val);
   rtl_add(&id_dest->val,&id_dest->val,&id_src->val);
   rtl_sr(id_dest->reg,&id_dest->val,id_dest->width);
   print_asm_template2(add);
