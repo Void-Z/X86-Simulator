d977d17436c50cf1edeb9fe496c84881bef0f8c7
tracer-ics2018 <tracer@njuics.org>
2018-12-19 16:33:18 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:33:18 up  4:22,  1 user,  load average: 0.36, 0.20, 0.19 c8c7b5ea449e40fcc2d66b6d85689bdffc629ff

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -209,7 +209,6 @@ make_DHelper(SI2E) {
   decode_op_rm(eip, id_dest, true, NULL, false);
   id_src->width = 1;
   decode_op_SI(eip, id_src, true);
-  printf("0x%08x",id_src->val);
   if (id_dest->width == 2) {
     id_src->val &= 0xffff;
   }
