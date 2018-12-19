da2324d9d807eb2844c9d3ac73c2b5f2a4502382
tracer-ics2018 <tracer@njuics.org>
2018-12-19 16:00:44 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:00:44 up  3:49,  1 user,  load average: 0.25, 0.26, 0.21 27b0a034d8601921f00ccab02f8290bf29f6e4aa

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -150,12 +150,17 @@ static inline void rtl_sr(int r, const rtlreg_t* src1, int width) {
 
 static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- ~src1
-  TODO();
+  *dest = ~*src1;
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  switch(width) {
+    case 4: *dest = *src1; break;
+    case 1: *dest = (*src1 & 0x80) == 1 ? *src1 | 0xffffff00 : *src1;break;
+    case 2: *dest = (*src1 & 0x8000) == 1 ? *src1 | 0xffff0000 : *src1;break;
+  }
+  
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
