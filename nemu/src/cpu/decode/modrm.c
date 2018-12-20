e47684a844ddc345b019785c29ff9116fca6da49
tracer-ics2018 <tracer@njuics.org>
2018-12-20 16:36:28 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:36:28 up  1:25,  1 user,  load average: 0.56, 0.36, 0.30 6799d0dbdac82578840f56883038567d946b1f7

--- nemu/src/cpu/decode/modrm.c
+++ nemu/src/cpu/decode/modrm.c
@@ -92,7 +92,7 @@ void read_ModR_M(vaddr_t *eip, Operand *rm, bool load_rm_val, Operand *reg, bool
 #ifdef DEBUG
     snprintf(reg->str, OP_STR_SIZE, "%%%s", reg_name(reg->reg, reg->width));
 #endif
-    printf("%%%s", reg_name(reg->reg, reg->width));
+    
   }
 
   if (m.mod == 3) {
@@ -105,7 +105,7 @@ void read_ModR_M(vaddr_t *eip, Operand *rm, bool load_rm_val, Operand *reg, bool
 #ifdef DEBUG
     sprintf(rm->str, "%%%s", reg_name(m.R_M, rm->width));
 #endif
-    printf("%%%s", reg_name(m.R_M, rm->width));
+    
   }
   else {
     load_addr(eip, &m, rm);
