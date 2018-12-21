3092f2898b84dd0451c7d5b08512a1381b9309dd
tracer-ics2018 <tracer@njuics.org>
2018-12-21 17:20:57 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:20:57 up  1:44,  1 user,  load average: 0.05, 0.12, 0.09 897decea2c6dc5a6b3df0f035832c51650bce8da

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -6,11 +6,11 @@ make_EHelper(test) {
   cpu.OF = 0;
   id_dest->val &= id_src->val;
   rtl_update_ZFSF(&id_dest->val,id_dest->width);
-  if(id_dest->type == OP_TYPE_REG) {
-    rtl_sr(id_dest->reg,&id_dest->val,id_dest->width);
-  } else {
-    rtl_sm(&id_dest->addr,&id_dest->val,id_dest->width);
-  }
+  // if(id_dest->type == OP_TYPE_REG) {
+  //   rtl_sr(id_dest->reg,&id_dest->val,id_dest->width);
+  // } else {
+  //   rtl_sm(&id_dest->addr,&id_dest->val,id_dest->width);
+  // }
   print_asm_template2(test);
 }
 
@@ -20,17 +20,20 @@ make_EHelper(and) {
   cpu.OF = 0;
   if(id_src->width == 1) {
     // id_src->val = (id_src->val & 0x80) == 0x80 ? id_src->val | 0xffffff00 : id_src->val;
-    rtl_sign_extend8to32(&id_dest->val);
+    rtl_sign_extend8to32(&id_src->val);
   }
   rtl_and(&id_dest->val,&id_dest->val,&id_src->val);
-  rtl_sr(id_dest->reg,&id_dest->val,id_dest->width);
+  operand_write(id_dest,&id_dest->val);
   rtl_update_ZFSF(&id_dest->val,id_dest->width);
   print_asm_template2(and);
 }
 
 make_EHelper(xor) {
+  if(id_src->width == 1) {
+    rtl_sign_extend8to32(&id_src->val);
+  }
   id_dest->val = id_dest->val ^ id_src->val;
-  rtl_sr(id_dest->reg,&id_dest->val,id_dest->width);
+  operand_write(id_dest,&id_dest->val);
   cpu.CF = 0;
   cpu.OF = 0;
   rtl_update_ZFSF(&id_dest->val,id_dest->width);
@@ -40,12 +43,11 @@ make_EHelper(xor) {
 make_EHelper(or) {
   cpu.CF = 0;
   cpu.OF = 0;
-  id_dest->val |= id_src->val;
-  if(id_dest->type == OP_TYPE_REG) {
-    rtl_sr(id_dest->reg,&id_dest->val,id_dest->width);
-  } else {
-    rtl_sm(&id_dest->addr,&id_dest->val,id_dest->width);
+  if(id_src->width == 1) {
+    rtl_sign_extend8to32(&id_src->val);
   }
+  id_dest->val |= id_src->val;
+  operand_write(id_dest,&id_dest->val);
   rtl_update_ZFSF(&id_dest->val,id_dest->width);
   print_asm_template2(or);
 }
@@ -97,20 +99,7 @@ make_EHelper(setcc) {
 }
 
 make_EHelper(not) {
-  switch(id_dest->width) {
-    case 1: {
-      id_dest->val = id_dest->val ^ 0xff;
-      break;
-    }
-    case 2: {
-      id_dest->val = id_dest->val ^ 0xffff;
-      break;
-    }
-    case 4: {
-      id_dest->val = id_dest->val ^ 0xffffffff;
-      break;
-    }
-  }
+  id_dest->val = ~id_dest->val;
   operand_write(id_dest,&id_dest->val);
   print_asm_template1(not);
 }
