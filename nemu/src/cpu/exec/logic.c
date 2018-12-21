5a0ce8b25b66b555d3cc4c5f5e687498f758a68c
tracer-ics2018 <tracer@njuics.org>
2018-12-21 17:00:12 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:00:12 up  1:23,  1 user,  load average: 0.05, 0.04, 0.06 d8dc99fcd4ea00b26455f7f215f10cf9a0f5955

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -97,7 +97,7 @@ make_EHelper(setcc) {
 }
 
 make_EHelper(not) {
-  id_dest->val = !id_dest->val;
+  id_dest->val = ~id_dest->val;
   operand_write(id_dest,&id_dest->val);
   print_asm_template1(not);
 }
