1be2a5ce9b4f94a0612f8b7fbc01674515760400
tracer-ics2018 <tracer@njuics.org>
2018-12-22 19:10:47 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  19:10:47 up  3:28,  1 user,  load average: 0.15, 0.16, 0.23 d19faddf45832c23cc860f3bd8d71c4634aaa107

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -47,7 +47,7 @@ make_EHelper(call) {
   // the target address is calculated at the decode stage
   rtl_push(&decoding.seq_eip,4);
   rtl_j(decoding.seq_eip + id_dest->val);
-  print_asm("call %x", decoding.jmp_eip);
+  print_asm("call %x", decoding.seq_eip + id_dest->val);
 }
 
 make_EHelper(ret) {
