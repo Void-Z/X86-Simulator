ecfb839054113e1662060ec3447373fae704bf85
tracer-ics2018 <tracer@njuics.org>
2018-12-23 15:13:19 +0800

    >  run U201514479 void Linux ubuntu 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  15:13:19 up  4:14,  1 user,  load average: 0.00, 0.09, 0.19 86538b82f1ac43ebb62d3a25b0fffc52e0f969c5

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -43,7 +43,7 @@ char* strcat(char* dst, const char* src) {
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  return -1;
   while(*s1 && *s2) {
     if(*s1 > *s2) {
       return 1;
