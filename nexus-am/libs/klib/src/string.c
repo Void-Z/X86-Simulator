76b933072c19299f56544c1b0be2137663a139f6
tracer-ics2018 <tracer@njuics.org>
2018-12-23 14:56:59 +0800

    >  run U201514479 void Linux ubuntu 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  14:56:59 up  3:58,  1 user,  load average: 0.47, 0.30, 0.30 6ade5df27e4a0adaefea09fe7fdb72decf84fac

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -85,15 +85,43 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  char *p = (char *)v;
+  while(n) {
+    *(p++) = (char)c;
+    --n;
+  }
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  char *p1 = (char *)out,*p2 = (char *)in;
+  while(n-- && *p2) {
+    *(p1++) = *(p2++);
+  }
+  return out;
 }
 
-int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+int memcmp(const void* p1, const void* p2, size_t n){
+  char *s1 = (char *)p1,*s2 = (char *)p2;
+  while(*s1 && *s2 && n) {
+    --n;
+    if(*s1 > *s2) {
+      return 1;
+    } else if(*s1 < *s2) {
+      return -1;
+    } else {
+      ++s1;
+      ++s2;
+    }
+  }
+  if(!n) return 0;
+  if(*s1) {
+    return 1;
+  } else if(*s2) {
+    return -1;
+  } else {
+    return 0;
+  }
 }
 
 #endif
