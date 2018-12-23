86bf267997fed4e01c6e3be7c1525fd2b221c1ab
tracer-ics2018 <tracer@njuics.org>
2018-12-23 14:47:53 +0800

    >  run U201514479 void Linux ubuntu 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  14:47:53 up  3:49,  1 user,  load average: 0.13, 0.27, 0.33 f0dc066d51848283a121695b71788717275070d

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,27 +3,85 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t len = 0;
+  while(*s) {
+    ++s;
+    ++len;
+  }
+  return len;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  char *p = dst;
+  while(*src) {
+    *(dst++) = *(src++);
+  }
+  *dst = 0;
+  return p;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  char *p = dst;
+  while(*src && n) {
+    *(dst++) = *(src++);
+    --n;
+  }
+  *dst = 0;
+  return p;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  char *p = dst;
+  while(*dst) {
+    ++dst;
+  }
+  while(*src) {
+    *(dst++) = *(src++);
+  }
+  *dst = 0;
+  return p;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  while(*s1 && *s2) {
+    if(*s1 > *s2) {
+      return 1;
+    } else if(*s1 < *s2) {
+      return -1;
+    } else {
+      ++s1;
+      ++s2;
+    }
+  }
+  if(*s1) {
+    return 1;
+  } else if(*s2) {
+    return -1;
+  } else {
+    return 0;
+  }
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+  while(*s1 && *s2 && n) {
+    --n;
+    if(*s1 > *s2) {
+      return 1;
+    } else if(*s1 < *s2) {
+      return -1;
+    } else {
+      ++s1;
+      ++s2;
+    }
+  }
+  if(!n) return 0;
+  if(*s1) {
+    return 1;
+  } else if(*s2) {
+    return -1;
+  } else {
+    return 0;
+  }
 }
 
 void* memset(void* v,int c,size_t n) {
