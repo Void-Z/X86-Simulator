12e08a2e51476a0a7781344421744da9e5113373
tracer-ics2018 <tracer@njuics.org>
2018-12-23 22:40:14 +0800

    >  run U201514479 void Linux ubuntu 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  22:40:14 up  9:31,  1 user,  load average: 0.21, 0.24, 0.24 3bcf4921f96da228d493da4d5fd8e7b848ff20d9

--- nexus-am/am/arch/x86-nemu/src/devices/timer.c
+++ nexus-am/am/arch/x86-nemu/src/devices/timer.c
@@ -6,9 +6,10 @@ size_t timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _UptimeReg *uptime = (_UptimeReg *)buf;
-      printf("----%d----\n",inl(0x48));
-      uptime->hi = inl(0x48);
-      uptime->lo = inl(0x49);
+      uint32_t a = inl(0x48);
+      _putc((char)a);
+      // uptime->hi = inl(0x48);
+      // uptime->lo = inl(0x49);
       return uptime->lo;
     }
     case _DEVREG_TIMER_DATE: {
