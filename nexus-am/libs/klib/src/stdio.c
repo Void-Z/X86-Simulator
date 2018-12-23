042d2f8376329bc65e485e17ba5005a4e9ba8aee
tracer-ics2018 <tracer@njuics.org>
2018-12-23 17:16:38 +0800

    >  run U201514479 void Linux ubuntu 4.15.0-42-generic #45-Ubuntu SMP Thu Nov 15 19:32:57 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:16:38 up  6:18,  1 user,  load average: 0.37, 0.25, 0.20 34a5207cc04bbf322d73066dc6566d79fd4e0d56

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -12,17 +12,24 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  int f = 0,d;
+  int f = 0,d,n;
+  char buf[32];
   const char * s;
+  char *p = out;
+  
   va_list arg_ptr;
   va_start(arg_ptr,0);
   while(*fmt) {
     if(f) {
       if(*fmt == 'd') {
         d = va_arg(arg_ptr,int);
+        n = 0;
         while(d) {
-          *(out++) = d % 10 + '0';
+          *(buf + n++) = d % 10 + '0';
           d /= 10;
+        }
+        while(n--) {
+          *(out++) = *(buf + n);
         }
       } else if(*fmt == 's') {
         s = va_arg(arg_ptr,const char *);
@@ -42,7 +49,7 @@ int sprintf(char *out, const char *fmt, ...) {
   }
   *out = 0;
   va_end(arg_ptr);
-  return 0;
+  return out - p;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
