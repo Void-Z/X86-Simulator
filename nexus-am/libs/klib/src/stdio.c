4ad9eaf2e3d180dc8e43f9fafd2ea9c06b2be044
tracer-ics2018 <tracer@njuics.org>
2018-12-24 14:34:15 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  14:34:15 up  3:08,  1 user,  load average: 0.29, 0.42, 0.38 f3b61615d03cce4b55ebcc935ad9658bb500b3b

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -13,46 +13,6 @@ int printf(const char *fmt, ...) {
     _putc(*(p++));
   }
   return n;
-
-  // int f = 0,d,n,count = 0;
-  // char buf[32];
-  // const char * s;
-  
-  // va_list arg_ptr;
-  // va_start(arg_ptr,0);
-  // while(*fmt) {
-  //   if(f) {
-  //     if(*fmt == 'd') {
-  //       d = va_arg(arg_ptr,int);
-  //       n = 0;
-  //       while(d) {
-  //         *(buf + n++) = d % 10 + '0';
-  //         d /= 10;
-  //       }
-  //       while(n--) {
-  //         _putc(*(buf + n));
-  //         ++count;
-  //       }
-  //     } else if(*fmt == 's') {
-  //       s = va_arg(arg_ptr,const char *);
-  //       while(*s) {
-  //         _putc(*(s++));
-  //         ++count;
-  //       }
-  //     }
-  //     f = 0;
-  //   } else {
-  //     if(*fmt == '%') {
-  //       f = 1;
-  //     } else {
-  //       _putc(*fmt);
-  //       ++count;
-  //     }
-  //   }
-  //   ++fmt;
-  // }
-  // va_end(arg_ptr);
-  // return count;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
@@ -99,44 +59,6 @@ int sprintf(char *out, const char *fmt, ...) {
   int n = vsprintf(out,fmt,arg_ptr);
   va_end(arg_ptr);
   return n;
-  // int f = 0,d,n;
-  // char buf[32];
-  // const char * s;
-  // char *p = out;
-  
-  // va_list arg_ptr;
-  // va_start(arg_ptr,0);
-  // while(*fmt) {
-  //   if(f) {
-  //     if(*fmt == 'd') {
-  //       d = va_arg(arg_ptr,int);
-  //       n = 0;
-  //       while(d) {
-  //         *(buf + n++) = d % 10 + '0';
-  //         d /= 10;
-  //       }
-  //       while(n--) {
-  //         *(out++) = *(buf + n);
-  //       }
-  //     } else if(*fmt == 's') {
-  //       s = va_arg(arg_ptr,const char *);
-  //       while(*s) {
-  //         *(out++) = *(s++);
-  //       }
-  //     }
-  //     f = 0;
-  //   } else {
-  //     if(*fmt == '%') {
-  //       f = 1;
-  //     } else {
-  //       *(out++) = *fmt;
-  //     }
-  //   }
-  //   ++fmt;
-  // }
-  // *out = 0;
-  // va_end(arg_ptr);
-  // return out - p;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
