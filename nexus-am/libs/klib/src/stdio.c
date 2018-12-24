2977b526d229d63168960eb713bf25511928cf54
tracer-ics2018 <tracer@njuics.org>
2018-12-24 14:21:39 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  14:21:39 up  2:56,  1 user,  load average: 0.30, 0.24, 0.22 9b29f54ca8a5911d69c2158e71c5d53bb01ce723

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -4,63 +4,67 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  int f = 0,d,n,count = 0;
-  char buf[32];
-  const char * s;
-  
+  char buf[256],*p = buf;
   va_list arg_ptr;
-  va_start(arg_ptr,0);
-  while(*fmt) {
-    if(f) {
-      if(*fmt == 'd') {
-        d = va_arg(arg_ptr,int);
-        n = 0;
-        while(d) {
-          *(buf + n++) = d % 10 + '0';
-          d /= 10;
-        }
-        while(n--) {
-          _putc(*(buf + n));
-          ++count;
-        }
-      } else if(*fmt == 's') {
-        s = va_arg(arg_ptr,const char *);
-        while(*s) {
-          _putc(*(s++));
-          ++count;
-        }
-      }
-      f = 0;
-    } else {
-      if(*fmt == '%') {
-        f = 1;
-      } else {
-        _putc(*fmt);
-        ++count;
-      }
-    }
-    ++fmt;
-  }
+  va_start(arg_ptr,fmt);
+  int n = vsprintf(buf,fmt,arg_ptr);
   va_end(arg_ptr);
-  return count;
+  while(*p) {
+    _putc(*(p++));
+  }
+  return n;
+
+  // int f = 0,d,n,count = 0;
+  // char buf[32];
+  // const char * s;
+  
+  // va_list arg_ptr;
+  // va_start(arg_ptr,0);
+  // while(*fmt) {
+  //   if(f) {
+  //     if(*fmt == 'd') {
+  //       d = va_arg(arg_ptr,int);
+  //       n = 0;
+  //       while(d) {
+  //         *(buf + n++) = d % 10 + '0';
+  //         d /= 10;
+  //       }
+  //       while(n--) {
+  //         _putc(*(buf + n));
+  //         ++count;
+  //       }
+  //     } else if(*fmt == 's') {
+  //       s = va_arg(arg_ptr,const char *);
+  //       while(*s) {
+  //         _putc(*(s++));
+  //         ++count;
+  //       }
+  //     }
+  //     f = 0;
+  //   } else {
+  //     if(*fmt == '%') {
+  //       f = 1;
+  //     } else {
+  //       _putc(*fmt);
+  //       ++count;
+  //     }
+  //   }
+  //   ++fmt;
+  // }
+  // va_end(arg_ptr);
+  // return count;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
-}
-
-int sprintf(char *out, const char *fmt, ...) {
   int f = 0,d,n;
   char buf[32];
   const char * s;
   char *p = out;
-  
-  va_list arg_ptr;
-  va_start(arg_ptr,0);
+
   while(*fmt) {
     if(f) {
       if(*fmt == 'd') {
-        d = va_arg(arg_ptr,int);
+        d = va_arg(ap,int);
         n = 0;
         while(d) {
           *(buf + n++) = d % 10 + '0';
@@ -70,7 +74,7 @@ int sprintf(char *out, const char *fmt, ...) {
           *(out++) = *(buf + n);
         }
       } else if(*fmt == 's') {
-        s = va_arg(arg_ptr,const char *);
+        s = va_arg(ap,const char *);
         while(*s) {
           *(out++) = *(s++);
         }
@@ -86,8 +90,53 @@ int sprintf(char *out, const char *fmt, ...) {
     ++fmt;
   }
   *out = 0;
-  va_end(arg_ptr);
   return out - p;
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  va_list arg_ptr;
+  va_start(arg_ptr,fmt);
+  int n = vsprintf(out,fmt,arg_ptr);
+  va_end(arg_ptr);
+  return n;
+  // int f = 0,d,n;
+  // char buf[32];
+  // const char * s;
+  // char *p = out;
+  
+  // va_list arg_ptr;
+  // va_start(arg_ptr,0);
+  // while(*fmt) {
+  //   if(f) {
+  //     if(*fmt == 'd') {
+  //       d = va_arg(arg_ptr,int);
+  //       n = 0;
+  //       while(d) {
+  //         *(buf + n++) = d % 10 + '0';
+  //         d /= 10;
+  //       }
+  //       while(n--) {
+  //         *(out++) = *(buf + n);
+  //       }
+  //     } else if(*fmt == 's') {
+  //       s = va_arg(arg_ptr,const char *);
+  //       while(*s) {
+  //         *(out++) = *(s++);
+  //       }
+  //     }
+  //     f = 0;
+  //   } else {
+  //     if(*fmt == '%') {
+  //       f = 1;
+  //     } else {
+  //       *(out++) = *fmt;
+  //     }
+  //   }
+  //   ++fmt;
+  // }
+  // *out = 0;
+  // va_end(arg_ptr);
+  // return out - p;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
