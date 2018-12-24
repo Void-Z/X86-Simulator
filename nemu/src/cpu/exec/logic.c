92320a882b1e5941ffb6febc38b0730b5ebff7ea
tracer-ics2018 <tracer@njuics.org>
2018-12-24 11:43:59 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:43:59 up  2:11,  1 user,  load average: 0.23, 0.81, 0.79 bc466a2c747fc200f03042039fa584b5849c926a

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -86,9 +86,12 @@ make_EHelper(shl) {
 }
 
 make_EHelper(shr) {
+  printf("0x%08x >> %d = ",id_dest->val,id_src->val);
   for(t0 = 0;t0 < id_src->val;++t0) {
     id_dest->val = id_dest->val / 2;
+    printf("0x%08x, ",id_dest->val);
   }
+   printf("\n");
   operand_write(id_dest,&id_dest->val);
   rtl_update_ZFSF(&id_dest->val,id_dest->width);
   // unnecessary to update CF and OF in NEMU
