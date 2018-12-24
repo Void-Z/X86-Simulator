5cd1667845f0ff3e1e7a63717b0a62385619e6c4
tracer-ics2018 <tracer@njuics.org>
2018-12-24 14:55:45 +0800

    >  run U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  14:55:45 up  3:30,  1 user,  load average: 0.12, 0.28, 0.29 3a37f611c78ce5036f1acc9aecdd629bb7d66ac8

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -6,8 +6,14 @@ size_t input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
+      // kbd->keydown = 0;
+      // kbd->keycode = _KEY_NONE;
+      kbd->keycode = inl(0x60);
+      if((kbd->keycode & 0x8000)) {
+        kbd->keydown = 1;
+      } else {
+        kbd->keydown = 0;
+      }
       return sizeof(_KbdReg);
     }
   }
