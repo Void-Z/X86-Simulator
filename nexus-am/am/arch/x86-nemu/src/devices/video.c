eae4974ffa56c2c7dc1ffc7115d789ce2c4c6e7f
tracer-ics2018 <tracer@njuics.org>
2018-12-24 15:24:27 +0800

    >  run U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  15:24:27 up  3:56,  1 user,  load average: 0.60, 0.41, 0.28 3e1b23b0322cb008f6035c946c7defc231eb90d5

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -23,12 +23,13 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
       for(int x = ctl->x;x < ctl->w;++x) {
         for(int y = ctl->y;y < ctl->h;++y) {
-          outl(0x40000 + y * 400 + x,ctl->pixels[x - ctl->w + (y - ctl->h) * 400]);
+          // outl(0x40000 + y * 400 + x,ctl->pixels[x - ctl->w + (y - ctl->h) * 400]);
+          fb[y * 400 + x] = ctl->pixels[x - ctl->w + (y - ctl->h) * 400];
         }
       }
-      // int i;
-      // int size = screen_width() * screen_height();
-      // for (i = 0; i < size; i ++) fb[i] = i;
+      int i;
+      int size = screen_width() * screen_height();
+      for (i = 0; i < size; i ++) fb[i] = i;
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }
