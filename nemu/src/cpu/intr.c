770c9c926af4f85d63be48fbb499f091c36e4ee3
tracer-ics2018 <tracer@njuics.org>
2018-12-26 11:06:49 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:06:49 up  7:23,  1 user,  load average: 0.09, 0.27, 0.32 b12c7ad183d95d93bfb825d225ede04aab780f72

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,7 +6,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
   GateDesc idt;
-  idt = (GateDesc)vaddr_read(ret_addr,NO);
+  idt = (GateDesc)vaddr_read(ret_addr + NO * 4,4);
   printf("%d 0x%08x 0x%08x\n",NO,idt.offset_15_0,idt.offset_31_16);
   rtl_j((idt.offset_31_16 << 16) + idt.offset_15_0);
 }
