cb0d878b27b64b224225bdd13113427371d8fbb9
tracer-ics2018 <tracer@njuics.org>
2018-12-28 18:44:58 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  18:44:58 up  2:28,  1 user,  load average: 0.41, 0.36, 0.27 b9bb7b325feaaea17fd5ae8645aaddcd7cdca05

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -11,10 +11,13 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   GateDesc idt;
   printf("cpu idtr : 0x%08x\n",cpu.idtr);
   for(int i = 0;i <= NO;++i) {
-    *(uint32_t *)&idt = vaddr_read(cpu.idtr + NO * 8,4);
-    *((uint32_t *)&idt + 1) = vaddr_read(cpu.idtr + NO * 8 + 4,4);
-    printf("int %d addr : 0x%08x\n",NO,(idt.offset_31_16 << 16) + idt.offset_15_0);
+    *(uint32_t *)&idt = vaddr_read(cpu.idtr + i * 8,4);
+    *((uint32_t *)&idt + 1) = vaddr_read(cpu.idtr + i * 8 + 4,4);
+    printf("int %d addr : 0x%08x\n",i,(idt.offset_31_16 << 16) + idt.offset_15_0);
   }
+  // *(uint32_t *)&idt = vaddr_read(cpu.idtr + NO * 8,4);
+  // *((uint32_t *)&idt + 1) = vaddr_read(cpu.idtr + NO * 8 + 4,4);
+  // printf("int %d addr : 0x%08x\n",NO,(idt.offset_31_16 << 16) + idt.offset_15_0);
   rtl_j((idt.offset_31_16 << 16) + idt.offset_15_0);
 }
 
