494ef1b84ff103e72ddccb11525e0d5afb2fd604
tracer-ics2018 <tracer@njuics.org>
2018-12-28 17:10:52 +0800

    >  run U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:10:52 up  1:57,  1 user,  load average: 0.60, 0.38, 0.22 6aa228d957e3cb975a540c91877acf11efae07b

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,10 @@
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
+    case 1: {
+      printf("irq 1\n");
+      break;
+    }
     default: panic("Unhandled event ID = %d", e.event);
   }
 
