c3d6f2367cf91c0ed40852c13ed6b1007e1d124e
tracer-ics2018 <tracer@njuics.org>
2019-01-02 12:40:38 +0800

    >  run U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  12:40:38 up  5:21,  1 user,  load average: 0.55, 0.34, 0.27 ad21cf8e2cfefb231a1553dcd5454fd6190caf0

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,9 +1,10 @@
 #include "common.h"
-
+_Context* do_syscall(_Context *c);
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case _EVENT_SYSCALL: {
-      printf("syscall,id = %d\n",e.event);
+      // printf("syscall,id = %d\n",e.event);
+      do_syscall(c);
       break;
     }
     case _EVENT_ERROR: {
