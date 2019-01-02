846a6911ab755a2da785bf842796895bfca4ec5c
tracer-ics2018 <tracer@njuics.org>
2019-01-02 11:51:20 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:51:20 up  4:32,  1 user,  load average: 0.27, 0.28, 0.27 648a2aa2e67b90d1f9a86c24b07ff1e438bcb77b

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,21 +5,24 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+  union {
+    GateDesc GD;
+    struct {
+      uint32_t low,high;
+    };
+  } idt;
   rtl_push(&cpu.EFLAGS,4);
   rtl_push(&cpu.cs,4);
   rtl_push(&ret_addr,4);
-  GateDesc idt;
-  // printf("cpu idtr : 0x%08x\n",cpu.idtr);
-  // for(int i = 0;i <= NO + 0x82;++i) {
-  //   *(uint32_t *)&idt = vaddr_read(cpu.idtr + i * 8,4);
-  //   *((uint32_t *)&idt + 1) = vaddr_read(cpu.idtr + i * 8 + 4,4);
-  //   printf("int %d addr : 0x%08x\n",i,(idt.offset_31_16 << 16) + idt.offset_15_0);
-  // }
-  NO += 0;
-  *(uint32_t *)&idt = vaddr_read(cpu.idtr + NO * 8,4);
-  *((uint32_t *)&idt + 1) = vaddr_read(cpu.idtr + NO * 8 + 4,4);
-  // printf("int %d addr : 0x%08x\n",NO,(idt.offset_31_16 << 16) + idt.offset_15_0);
-  rtl_j((idt.offset_31_16 << 16) + idt.offset_15_0);
+  vaddr_t addr = cpu.idtr + 8 * NO;
+  idt.low = vaddr_read(addr,4);
+  idt.high = vaddr_read(addr + 4,4);
+  t0 = ((idt.GD.offset_31_16 & 0xffff) << 16) | (idt.GD.offset_15_0 & 0xffff);
+  rtl_j(t0);
+  // GateDesc idt;
+  // *(uint32_t *)&idt = vaddr_read(cpu.idtr + NO * 8,4);
+  // *((uint32_t *)&idt + 1) = vaddr_read(cpu.idtr + NO * 8 + 4,4);
+  // rtl_j((idt.offset_31_16 << 16) + idt.offset_15_0);
 }
 
 void dev_raise_intr() {
