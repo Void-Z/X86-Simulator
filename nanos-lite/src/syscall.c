391a17f8bcae5e0b40d69ae85ff8b87db012a214
tracer-ics2018 <tracer@njuics.org>
2019-01-02 12:47:42 +0800

    >  run U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  12:47:42 up  5:28,  1 user,  load average: 0.35, 0.35, 0.29 ca26c9fdbf8660d7d22e00a9ef779c05f6a65d48

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,13 +5,22 @@ void sys_yield() {
   _yield();
 }
 
+void sys_exit(int code) {
+  _halt(code);
+}
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;  
+  a[3] = c->GPR4;
 
   switch (a[0]) {
-    case 0:
+    case 0: {
+      sys_exit(a[1]);
+      break;
+    }
     case 1: {
       sys_yield();
       break;
