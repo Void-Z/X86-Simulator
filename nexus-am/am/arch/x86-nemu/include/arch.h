522e2bfb49703f2705e94cb6d084801c4321ddff
tracer-ics2018 <tracer@njuics.org>
2019-01-02 11:37:53 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:37:53 up  4:19,  1 user,  load average: 0.07, 0.07, 0.17 69125eea94e7ec6606b6e4e43817122bf4f3b0

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -8,11 +8,6 @@
 
 typedef uint32_t size_t;
 
-// struct _Context {
-//   uintptr_t esi, ebx, eax, eip, edx, err, eflags, ecx, cs, esp, edi, ebp;
-//   struct _Protect *prot;
-//   int       irq;
-// };
 
 struct _Context {
   struct _Protect *prot;
