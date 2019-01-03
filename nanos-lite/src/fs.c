8abf724bc0051be2a7c50e400ab4ab1abce531a3
tracer-ics2018 <tracer@njuics.org>
2019-01-03 16:12:29 +0800

    >  run U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:12:29 up  1:22,  1 user,  load average: 0.51, 0.90, 0.87 e1073e7f27cb31c44fb93eb35e3cf8d13ce00abc

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -125,12 +125,14 @@ ssize_t fs_read(int fd, void *buf, size_t len) {
       break;
     }
     default: {
+      
       if(file_table[fd].open_offset + len >= fs_filesz(fd)) {
         len = fs_filesz(fd) - file_table[fd].open_offset - 1;
       }
       size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
       int i = 0;
       i = ramdisk_read(buf,offset,len);
+      printf("%s\n",buf);
       file_table[fd].open_offset += i;
       return i;
     }
