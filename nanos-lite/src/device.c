9421def2a1782ddb13e1d807affa6c554aad8ef3
tracer-ics2018 <tracer@njuics.org>
2019-01-03 12:44:40 +0800

    >  run U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  12:44:40 up  2:31,  1 user,  load average: 0.34, 0.50, 0.42 8617ce60d78d681c1a5843d0ac50b9769009f5e3

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -2,9 +2,9 @@
 #include <amdev.h>
 #include "fs.h"
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  for(int i = 0;i < len;++i) {
-    _putc(*((char *)buf + i));
-  }
+  // for(int i = 0;i < len;++i) {
+  //   _putc(*((char *)buf + i));
+  // }
   return len;
 }
 
@@ -23,30 +23,32 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  int i;
-  for(i = 0;i < len && i + offset < fs_filesz(FD_DISPINFO);++i) {
-    *((char *)buf + i) = dispinfo[i];
-  }
-  return i;
+  // int i;
+  // for(i = 0;i < len && i + offset < fs_filesz(FD_DISPINFO);++i) {
+  //   *((char *)buf + i) = dispinfo[i];
+  // }
+  // return i;
+  return 0;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t length) {
-  int h,w,len = length;
-  h = offset / screen_width();
-  w = offset % screen_width();
-  if(w + len <= screen_width()) {
-    draw_rect((uint32_t *)buf,w,h,len,1);
-  } else {
-    len -= screen_width() - w;
-    int height = len / screen_width(),width = len % screen_width();
-    if(height) {
-      draw_rect((uint32_t *)buf + screen_width() - w,0,h + 1,screen_width(),height);
-    }
-    if(width) {
-      draw_rect((uint32_t *)buf + length - width,0,h + 1 + height,width,1);
-    }
-  }
-  return length;
+  // int h,w,len = length;
+  // h = offset / screen_width();
+  // w = offset % screen_width();
+  // if(w + len <= screen_width()) {
+  //   draw_rect((uint32_t *)buf,w,h,len,1);
+  // } else {
+  //   len -= screen_width() - w;
+  //   int height = len / screen_width(),width = len % screen_width();
+  //   if(height) {
+  //     draw_rect((uint32_t *)buf + screen_width() - w,0,h + 1,screen_width(),height);
+  //   }
+  //   if(width) {
+  //     draw_rect((uint32_t *)buf + length - width,0,h + 1 + height,width,1);
+  //   }
+  // }
+  // return length;
+  return 0;
 }
 
 void init_device() {
