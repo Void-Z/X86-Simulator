b281349ce95ab0ef3891329ca24f8c84b8965335
tracer-ics2018 <tracer@njuics.org>
2019-01-03 16:49:10 +0800

    >  run U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:49:10 up  1:59,  1 user,  load average: 0.94, 0.64, 0.52 7659f33e63aef18df55bd79288bd1fdd5eaaae93

--- navy-apps/tests/events/events.c
+++ navy-apps/tests/events/events.c
@@ -9,7 +9,6 @@ int main() {
       char buf[256];
       char *p = buf, ch;
       while ((ch = fgetc(fp)) != -1) {
-        printf("get\n");
         *p ++ = ch;
         if(ch == '\n') {
           *p = '\0';
