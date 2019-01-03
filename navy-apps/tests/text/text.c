222d15005a866d32690b5d7b271c036e9dbd9c10
tracer-ics2018 <tracer@njuics.org>
2019-01-03 09:40:55 +0800

    >  run U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  09:40:55 up 42 min,  1 user,  load average: 0.23, 0.23, 0.34 2a1d675de958cbbd1766df1d515e372c16509c41

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -1,3 +1,44 @@
+// #include <stdio.h>
+// #include <assert.h>
+
+// int main() {
+//   FILE *fp = fopen("/share/texts/num", "r+");
+//   assert(fp);
+
+//   fseek(fp, 0, SEEK_END);
+//   long size = ftell(fp);
+//   assert(size == 5000);
+
+//   fseek(fp, 500 * 5, SEEK_SET);
+//   int i, n;
+//   for (i = 500; i < 1000; i ++) {
+//     fscanf(fp, "%d", &n);
+//     assert(n == i + 1);
+//   }
+
+//   fseek(fp, 0, SEEK_SET);
+//   for (i = 0; i < 500; i ++) {
+//     fprintf(fp, "%4d\n", i + 1 + 1000);
+//   }
+
+//   for (i = 500; i < 1000; i ++) {
+//     fscanf(fp, "%d", &n);
+//     assert(n == i + 1);
+//   }
+
+//   fseek(fp, 0, SEEK_SET);
+//   for (i = 0; i < 500; i ++) {
+//     fscanf(fp, "%d", &n);
+//     assert(n == i + 1 + 1000);
+//   }
+
+//   fclose(fp);
+
+//   printf("PASS!!!\n");
+
+//   return 0;
+// }
+
 #include <stdio.h>
 #include <assert.h>
 
@@ -10,17 +51,24 @@ int main() {
   assert(size == 5000);
 
   fseek(fp, 500 * 5, SEEK_SET);
+  size = ftell(fp);
+  printf("55:%d\n",size);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
+    size = ftell(fp);
+    printf("60:%d\n",size);
     assert(n == i + 1);
   }
 
   fseek(fp, 0, SEEK_SET);
+  size = ftell(fp);
+  printf("66:%d\n",size);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
-
+  size = ftell(fp);
+  printf("71:%d\n",size);
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
