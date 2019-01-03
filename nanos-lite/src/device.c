546cefd4546194fa20300adca07a6cfcedaf0b41
tracer-ics2018 <tracer@njuics.org>
2019-01-03 15:05:50 +0800

    >  run U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  15:05:50 up 16 min,  1 user,  load average: 0.70, 1.09, 0.83 e9574c8eb069354fd2ec6e2312909caf870763cb

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -29,6 +29,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   } else {
     sprintf(buf,"%s %s\n",down ? "kd" : "ku",keyname[key]);
   }
+  printf("%s",buf);
   return strlen(buf);
 }
 
--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -94,10 +94,9 @@ int NDL_WaitEvent(NDL_Event *event) {
   while (1) {
     while ((ch = getc(evtdev)) != -1) {
       *p ++ = ch;
-      printf("p-buf:%d,sizebuf:%d\n",p-buf,sizeof(buf));
-      printf("ch:%c",ch);
-      // assert(p - buf < sizeof(buf));
-      if(p - buf >= sizeof(buf)) break;
+      printf("p-buf:%d,sizebuf:%d\t",p-buf,sizeof(buf));
+      printf("ch:%c\n",ch);
+      assert(p - buf < sizeof(buf));
       if (ch == '\n') break;
     }
 
