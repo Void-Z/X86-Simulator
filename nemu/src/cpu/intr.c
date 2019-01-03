82e84b952587a3929236919fe925bcc11bd76d31
tracer-ics2018 <tracer@njuics.org>
2019-01-03 13:42:00 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  13:42:00 up  3:28,  1 user,  load average: 0.49, 0.32, 0.30 e0ca3c1b4b58233e917ea4621ad210cbe1c5bb6

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -11,9 +11,6 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_push(&ret_addr,4);
   *(uint32_t *)&idt = vaddr_read(cpu.idtr + NO * 8,4);
   *((uint32_t *)&idt + 1) = vaddr_read(cpu.idtr + NO * 8 + 4,4);
-  Log("\n0x%08x\n",*(uint32_t *)&idt);
-  Log("\n0x%08x\n",*((uint32_t *)&idt+1));
-  Log("\nraise intr:0x%08x\n",(idt.offset_31_16 << 16) + idt.offset_15_0);
   rtl_j((idt.offset_31_16 << 16) + idt.offset_15_0);
 }
 
