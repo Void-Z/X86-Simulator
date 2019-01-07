30af0f4f8950b292664e634d651ffd0f54f2da88
tracer-ics2018 <tracer@njuics.org>
2019-01-07 19:43:13 +0800

    >  compile U201514479 void Linux ubuntu 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  19:43:13 up  9:56,  1 user,  load average: 0.51, 0.41, 0.36 5d3534c497d263295356a187b83d5fb3f961fd7

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -13,7 +13,7 @@ bool interpret_relop(uint32_t relop, const rtlreg_t src1, const rtlreg_t src2);
 
 /* RTL basic instructions */
 
- inline void interpret_rtl_li(rtlreg_t* dest, uint32_t imm) {
+static inline void interpret_rtl_li(rtlreg_t* dest, uint32_t imm) {
   *dest = imm;
 }
 
@@ -81,7 +81,7 @@ static inline void interpret_rtl_lm(rtlreg_t *dest, const rtlreg_t* addr, int le
   *dest = vaddr_read(*addr, len);
 }
 
-static inline void interpret_rtl_sm(const rtlreg_t* addr, const rtlreg_t* src1, int len) {
+ inline void interpret_rtl_sm(const rtlreg_t* addr, const rtlreg_t* src1, int len) {
   vaddr_write(*addr, *src1, len);
 }
 
